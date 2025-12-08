dca68c046cc2652407bd4c33135ec40ccc754c45
GrSpirit <gr.spirit@gmail.com>
2025-12-08 15:14:19 +0300

    Day1

--- day01/src/task.rs
+++ day01/src/task.rs
@@ -1,63 +1,114 @@
-use std::collections::HashMap;
+use std::{fmt, str::FromStr};
 
 #[derive(thiserror::Error, Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Error {
     #[error("Parse error")]
-    ParseError
+    ParseError,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-pub fn task1<S: AsRef<str>>(lines: &[S]) -> Result<i32> {
-    let mut list1 = Vec::new();
-    let mut list2 = Vec::new();
-    for line in lines {
-        let nums = line.as_ref().split_whitespace().collect::<Vec<_>>();
-        list1.push(nums[0].parse::<i32>().map_err(|_| Error::ParseError)?);
-        list2.push(nums[1].parse::<i32>().map_err(|_| Error::ParseError)?);
+enum Command {
+    Left(i32),
+    Right(i32),
+}
+
+impl FromStr for Command {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self> {
+        match (&s[..1], s[1..].parse::<i32>()) {
+            ("L", Ok(x)) => Ok(Command::Left(x)),
+            ("R", Ok(x)) => Ok(Command::Right(x)),
+            _ => Err(Error::ParseError),
+        }
     }
-    list1.sort_unstable();
-    list2.sort_unstable();
-    let result = list1.iter().zip(list2.iter()).map(|(a, b)| (a - b).abs()).sum();
-    Ok(result)
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Command::Left(x) => write!(f, "L{}", x),
+            Command::Right(x) => write!(f, "R{}", x),
+        }
+    }
+}
+
+pub fn task1<S: AsRef<str>>(lines: &[S]) -> Result<i32> {
+    let mut pos = 50;
+    let mut ans = 0;
+    let module = 100;
+    let commands = lines
+        .iter()
+        .map(|s| s.as_ref().parse::<Command>())
+        .collect::<Result<Vec<Command>>>()?;
+    for cmd in commands {
+        match cmd {
+            Command::Left(n) => {
+                pos = (pos - n) % module;
+            }
+            Command::Right(n) => {
+                pos = ((pos + n) % module + module) % module;
+            }
+        }
+        if pos == 0 {
+            ans += 1;
+        }
+    }
+    Ok(ans)
 }
 
 pub fn task2<S: AsRef<str>>(lines: &[S]) -> Result<i32> {
-    let mut list1 = Vec::new();
-    let mut list2 = Vec::new();
-    for line in lines {
-        let nums = line.as_ref().split_whitespace().collect::<Vec<_>>();
-        list1.push(nums[0].parse::<i32>().map_err(|_| Error::ParseError)?);
-        list2.push(nums[1].parse::<i32>().map_err(|_| Error::ParseError)?);
+    let mut pos = 50;
+    let mut ans = 0;
+    let module = 100;
+    let commands = lines
+        .iter()
+        .map(|s| s.as_ref().parse::<Command>())
+        .collect::<Result<Vec<Command>>>()?;
+    for cmd in commands {
+        let next_pos = match cmd {
+            Command::Left(n) => pos - n,
+            Command::Right(n) => pos + n,
+        };
+        if next_pos == 0 {
+            ans += 1;
+        } else if next_pos > 99 {
+            ans += next_pos / 100;
+        } else if next_pos < 0 {
+            ans += (next_pos / -100) + if pos == 0 { 0 } else { 1 };
+        }
+        pos = (next_pos % module + module) % module;
+        // println!(
+        //     "cmd={}, next_pos={}, pos={}, ans={}",
+        //     cmd, next_pos, pos, ans
+        // );
     }
-    let mut freq = HashMap::new();
-    list2.iter().for_each(|&num| *freq.entry(num).or_insert(0) += 1);
-    let mut result = 0;
-    for x in list1 {
-        result += x * *freq.get(&x).unwrap_or(&0);
-    }
-    Ok(result)
+    Ok(ans)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const DATA: &str = r"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+    const DATA: &str = r"L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
     #[test]
     fn task1_test() {
         let lines = DATA.lines().collect::<Vec<_>>();
         let result = task1(&lines);
-        assert_eq!(Ok(11), result);
+        assert_eq!(Ok(3), result);
     }
     #[test]
     fn task2_test() {
         let lines = DATA.lines().collect::<Vec<_>>();
         let result = task2(&lines);
-        assert_eq!(Ok(31), result);
+        assert_eq!(Ok(6), result);
     }
 }
