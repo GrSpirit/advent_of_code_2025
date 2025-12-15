e1f47b537094afa8e1cbbd1242127070359db1b0
GrSpirit <gr.spirit@gmail.com>
2025-12-15 16:20:28 +0300

    Day5

--- day05/src/task.rs
+++ day05/src/task.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashMap, HashSet};
-
 #[derive(thiserror::Error, Debug, Clone, Eq, PartialEq)]
 pub enum Error {
     #[error("Parse error")]
@@ -10,85 +8,89 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-fn is_correct_order(nums: &[i32], depends_on: &HashMap<i32, Vec<i32>>) -> bool {
-    let mut visited: HashSet<i32> = HashSet::new();
-    for x in nums {
-        for d in depends_on.get(x).unwrap_or(&Vec::new()) {
-            if visited.contains(d) {
-                return false;
-            }
-        }
-        visited.insert(*x);
-    }
-    return true;
-}
-
 pub fn task1<S: AsRef<str>>(lines: &[S]) -> Result<i32> {
-    let split_pos = lines.iter().position(|s| s.as_ref().is_empty()).ok_or(Error::FormatError)?;
+    let split_pos = lines
+        .iter()
+        .position(|s| s.as_ref().is_empty())
+        .ok_or(Error::FormatError)?;
     let (pairs, nums) = (&lines[..split_pos], &lines[split_pos + 1..]);
-    let mut depends_on: HashMap<i32, Vec<i32>> = HashMap::new();
-    for pair in pairs {
-        let (a, b) = pair.as_ref().split_once('|').ok_or_else(|| Error::FormatError)?;
-        depends_on.entry(a.parse::<i32>()?).or_default().push(b.parse::<i32>()?);
-    }
-    let pages = nums.iter()
-        .map(|s| s.as_ref().split(',').map(|x| x.parse::<i32>().map_err(|e| Error::ParseError(e))).collect::<Result<Vec<_>>>())
-        .collect::<Result<Vec<_>>>()?;
-    let mut ans = 0;
-    for page in pages.iter() {
-        if is_correct_order(page, &depends_on) {
-            ans += page[page.len() / 2];
-        } else {
-        }
-    }
-    Ok(ans)
+    let mut ranges = pairs
+        .iter()
+        .map(|pair| {
+            let p = pair.as_ref().split_once('-').unwrap();
+            (p.0.parse::<u64>().unwrap(), p.1.parse::<u64>().unwrap())
+        })
+        .collect::<Vec<(u64, u64)>>();
+    ranges.sort();
+    let result = nums
+        .into_iter()
+        .map(|s| s.as_ref().parse::<u64>().unwrap())
+        .filter(|u| ranges.iter().any(|(start, end)| start <= u && u <= end))
+        .count();
+    Ok(result as _)
 }
 
-pub fn task2<S: AsRef<str>>(_lines: &[S]) -> Result<i32> {
-    Ok(0)
+pub fn task2<S: AsRef<str>>(lines: &[S]) -> Result<u64> {
+    let split_pos = lines
+        .iter()
+        .position(|s| s.as_ref().is_empty())
+        .ok_or(Error::FormatError)?;
+    let pairs = &lines[..split_pos];
+    let mut ranges = pairs
+        .iter()
+        .map(|pair| {
+            let p = pair.as_ref().split_once('-').unwrap();
+            (p.0.parse::<u64>().unwrap(), p.1.parse::<u64>().unwrap())
+        })
+        .collect::<Vec<(u64, u64)>>();
+    ranges.sort();
+    let mut unique_ranges: Vec<(u64, u64)> = Vec::new();
+    for r in ranges {
+        if let Some(last) = unique_ranges.pop() {
+            if last.1 >= r.0 {
+                if last.1 <= r.1 {
+                    unique_ranges.push((last.0, r.1));
+                } else {
+                    unique_ranges.push(last);
+                }
+            } else {
+                unique_ranges.push(last);
+                unique_ranges.push(r);
+            }
+        } else {
+            unique_ranges.push(r);
+        }
+    }
+    let result = unique_ranges
+        .into_iter()
+        .fold(0, |acc, r| acc + r.1 - r.0 + 1);
+    Ok(result)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const DATA: &str = r"47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+    const DATA: &str = r"3-5
+10-14
+16-20
+12-18
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
+1
+5
+8
+11
+17
+32";
     #[test]
     fn task1_test() {
         let lines = DATA.lines().collect::<Vec<_>>();
         let result = task1(&lines);
-        assert_eq!(Ok(143), result);
+        assert_eq!(Ok(3), result);
     }
     #[test]
     fn task2_test() {
         let lines = DATA.lines().collect::<Vec<_>>();
         let result = task2(&lines);
-        assert_eq!(Ok(0), result);
+        assert_eq!(Ok(14), result);
     }
 }
